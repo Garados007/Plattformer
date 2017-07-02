537829661e2c772c4b78d56e7ac4e9d91247f378
Max Brauer <ma.brauer@live.de>
2017-07-03 00:12:19 +0200

    FPS monitor added

--- Plattformer/UI/Form1.cs
+++ Plattformer/UI/Form1.cs
@@ -59,6 +59,10 @@ namespace Plattformer.UI
                 var start = Environment.TickCount;
                 SetupBuffer();
                 RenderFunction();
+#if DEBUG
+                if (MonitorFPS)
+                    DrawMonitorFPS(Buffer.Graphics, Environment.TickCount - start, 0, 60, 1, LastViewSize.Width);
+#endif
                 try { Buffer.Render(); }
                 catch
                 {
@@ -100,5 +104,41 @@ namespace Plattformer.UI
                 ToggleFullscreen();
             }
         }
+
+#if DEBUG
+        public static bool MonitorFPS = true;
+
+        float[] timebuffer = new float[50];
+        int timepos = 0;
+        float lastmaxb = 0;
+
+        private void DrawMonitorFPS(Graphics g, float fill, float min, float max, float step, int width)
+        {
+            timebuffer[timepos] = fill;
+            timepos = (timepos + 1) % timebuffer.Length;
+            var maxb = timebuffer[0];
+            for (int i = 1; i < timebuffer.Length; ++i)
+                maxb = Math.Max(maxb, timebuffer[i]);
+            if (maxb > lastmaxb) lastmaxb = maxb;
+            else lastmaxb = maxb =
+                maxb + (lastmaxb - maxb) * 0.7f;
+
+            width = Math.Min(width, 400);
+
+            var r = new Rectangle(10, 10, width - 20, 10);
+            var f = new RectangleF(10, 10,
+                r.Width * (fill - min) / (max - min), 10);
+            g.FillRectangle(maxb < max ? Brushes.LightGreen : Brushes.Red, f);
+            g.DrawRectangle(Pens.Green, r);
+            for (float v = min, i = 0; v <= max; v += step, i++)
+            {
+                var l = 10 + r.Width * (v - min) / (max - min);
+                g.DrawLine(Pens.Green, l, 20, l,
+                    25 + (i % 5 == 0 ? 5 : 0) + (i % 20 == 0 ? 5 : 0));
+            }
+            var maxbl = 10 + r.Width * (maxb - min) / (max - min);
+            g.DrawLine(Pens.Green, maxbl, 12, maxbl, 18);
+        }
+#endif
     }
 }
